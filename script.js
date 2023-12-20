fe7266256679365992094b5b7d049f84d88c0f8f
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-12-20 19:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", async () => {
   // Get the values from the input fields
   const textValue = textInput.value;
-  const delayValue = parseInt(delayInput.value, 10);
+  const delayValue = parseInt(delayInput.value * 1000, 10);
 
   // Validate input
    if (textValue.trim() === '' || isNaN(delayValue) || delayValue < 0) {
-  //   alert('Please enter valid text and delay values.');   
+     alert('Please enter valid text and delay values.');   
 	return;
    }
 
-  // output.innerText = '';
+  output.innerText = '';
 
   const waitForDelay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
