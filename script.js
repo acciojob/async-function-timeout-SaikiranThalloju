e2ee8cf21f6f31406e84f845f559e3ee66d3874a
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-12-20 19:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 // Your JS code here.
+// Your JS code here.
 const output = document.getElementById("output");
 const textInput = document.getElementById("text");
 const delayInput = document.getElementById("delay");
@@ -11,19 +12,25 @@ btn.addEventListener("click", async () => {
   const delayValue = parseInt(delayInput.value * 1000, 10);
 
   // Validate input
-   if (textValue.trim() === '' || isNaN(delayValue) || delayValue < 0) {
-     alert('Please enter valid text and delay values.');   
-	return;
-   }
+  if (textValue.trim() === '' || isNaN(delayValue) || delayValue < 0) {
+    alert('Please enter valid text and delay values.');
+    return;
+  }
 
+  // Clear previous content in the output div
   output.innerText = '';
 
+  // Define a function to wait for a specified delay
   const waitForDelay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+  // Use async/await to wait for the specified delay
   await waitForDelay(delayValue);
+
+  // Display the text after the delay
   output.innerText = textValue;
 });
 
+ 
 
 
 
